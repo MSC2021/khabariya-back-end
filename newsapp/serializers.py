8453200e1f8e3ddd7d22bfbd67bafa696ff74088
MSC2021 <chinmay12456789@gmail.com>
2021-01-18 01:33:44 +0530

    minor db changes

--- newsapp/serializers.py
+++ newsapp/serializers.py
@@ -1,13 +1,17 @@
 from .models import *
 from rest_framework import serializers
+import datetime
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+        lookup_field = 'title'
 
 class NewsSerializer(serializers.HyperlinkedModelSerializer):
     category = CategorySerializer(read_only=True, many=True)
+    timestamp = serializers.DateTimeField(format="%H")
+ 
     class Meta:
         model = NewsArticle
         fields = ['id','title','category','paragraph',"images",'timestamp']
@@ -16,5 +20,5 @@ class NewsSerializer(serializers.HyperlinkedModelSerializer):
 class VideoLinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = NewsArticle
-        fields = ['title','youtube_link']
+        fields = ['id','title','youtube_link']
         lookup_field = 'title'
